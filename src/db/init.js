2444c2e6371b3464aa82dac467c5649fd4e2b804
Paulo Neto <paulolemosfn@gmail.com>
2021-06-30 18:33:32 -0300

    Criaçao inicial e configuaraçao inicial da base de dados

---
+++ src/db/init.js
@@ -0,0 +1,24 @@
+const Database= require('./config');
+
+const initDb={
+    async init(){
+        const db = await Database(); /* a propriedade 'await' obriga o JS a esperar o retorno da função antes de executar a proxima linha */
+
+        /*Comandos na linguagem do SQL sempre são em LETRA MAIÚSCULA  */
+        await db.exec(`
+        CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`);  
+        /*Comandos na linguagem do SQL sempre são em LETRA MAIÚSCULA  */
+
+        await db.exec(`CREATE TABLE questions(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            check INT
+        )`);/*PRIMARY KEY é o comando que garante que o id solicitado vai ser unico*/
+    }
+}
+
+initDb.init();
+
