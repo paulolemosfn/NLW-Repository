90cc8ecfccc19e88849eff7317aec8c1287a9fcd
Paulo Neto <paulolemosfn@gmail.com>
2021-07-02 15:19:10 -0300

    Persistindo dados no SQlite

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,20 +5,31 @@ module.exports = {
     async create(req, res){
         const db = await Database();
         const pass= req.body.password
-        let roomId
+        let roomId;
+        let isRoom = true;
+        while(roomsExistIds){
+
+
+        }
 
         for (let i = 0; i < 6; i++) {
             i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
             roomId += Math.floor(Math.random() * 10).toString();
         }
+        /* verifica se o numero da sala já existe*/
+        const roomsExistIds = await db.all(`SELECT id FROM  rooms`)
+        isRoom = roomsExistIds.some(roomsExistId => roomsExistId === roomId);
 
-        await db.run(`INSERT INTO rooms(
-            id,
-            pass     
-        ) VALUES(
-            ${parseInt(roomId)}, 
-            ${pass}
-        )`); /*Primeiro você deve passar a ordem dos items na tabela e logo depois os valores dela.*/
+        if (!isRoom){
+            await db.run(`INSERT INTO rooms(
+                id,
+                pass     
+            ) VALUES(
+                ${parseInt(roomId)}, 
+                ${pass}
+            )`); /*Primeiro você deve passar a ordem dos items na tabela e logo depois os valores dela.*/
+        }
+        /*Insere a sala no banco*/
 
         await db.close();
 
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ server.use(express.static("public"));
 
 server.set('views', path.join(__dirname,'views'));
 
-server.use(express.urlencoded({extend:true})); /*midware, está implcito entre a rota e pra onde a rota leva. Decodifica o que ta vindo pela rota(através do "urlencoded") e entrega o resultado. */
+server.use(express.urlencoded({extended:true})); /*midware, está implcito entre a rota e pra onde a rota leva. Decodifica o que ta vindo pela rota(através do "urlencoded") e entrega o resultado. */
 
 server.use(route);
 
